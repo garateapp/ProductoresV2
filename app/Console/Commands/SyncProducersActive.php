e2c328e82e90874533083f4d1c9a578abd75aeb2
calgreenex <carlos.alvarez@greenex.cl>
2025-10-27 17:48:09 -0300

    .

--- app/Console/Commands/SyncProducersActive.php
+++ app/Console/Commands/SyncProducersActive.php
@@ -30,6 +30,9 @@ class SyncProducersActive extends Command
             ->table('ADM_P_Entidades')
             ->select(['id', 'rut', 'nombre', 'direccion', 'codigo_sag'])
             ->where('tipo_juridico', 1)
+            ->where('codigo_sag', '!=', null)
+            ->where('codigo_sag', '!=', '')
+            ->where('E.CP1', '1')
             ->get();
 
         $normalizeRut = static function ($rut) {
