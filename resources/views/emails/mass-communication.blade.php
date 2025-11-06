44e6ca54e735e7a7492490a0f25bad8dc8bb1a90
calgreenex <carlos.alvarez@greenex.cl>
2025-11-06 16:05:39 -0300

    .

--- resources/views/emails/mass-communication.blade.php
+++ resources/views/emails/mass-communication.blade.php
@@ -6,7 +6,7 @@
 </head>
 <body style="font-family: Arial, sans-serif; color: #1f2933; line-height: 1.6;">
     <p><img src="https://appgreenex.cl/img/logogreenex.png" alt="{{ $serviceName }}" style="display: block; margin: 0 auto; max-width: 100%; height: auto;"></p>
-
+    <p><strong>Comunicado - {{ $serviceName }}</strong>
     <p>{!! nl2br(e($messageBody)) !!}</p>
 
     <p style="margin-top: 2rem;">Saludos cordiales,<br>Greenex</p>
