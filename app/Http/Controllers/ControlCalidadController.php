9d1be3da39a51d8080d52fa14eefd9be7796fb88
calgreenex <carlos.alvarez@greenex.cl>
2025-11-16 16:28:53 -0300

    .

--- app/Http/Controllers/ControlCalidadController.php
+++ app/Http/Controllers/ControlCalidadController.php
@@ -1828,7 +1828,7 @@ public function previewPage(Recepcion $recepcion)
             // $html_tabla_color = $html_tabla_color ?: $this->buildColorCoverageTable($coverageColor);
         }
     }
-     if($recepcion->id_emisor=="7023"  && $recepcion->variedad=='Rainier'){
+     if($recepcion->id_emisor=="7023"  && ($recepcion->variedad=='Rainier' || $recepcion->variedad=='Santina')) {
                 $exporterName = 'Greenex SpA';
             }
         return compact(
