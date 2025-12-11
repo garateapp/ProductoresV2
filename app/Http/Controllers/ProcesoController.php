24d12253286d4773a76b55be5b5561e35f6724d4
calgreenex <carlos.alvarez@greenex.cl>
2025-12-11 10:33:16 -0300

    .

--- app/Http/Controllers/ProcesoController.php
+++ app/Http/Controllers/ProcesoController.php
@@ -260,10 +260,10 @@ class ProcesoController extends Controller
         //         'error' => $e->getMessage(),
         //     ]);
 
-            return response()->json([
-                'message' => 'No se pudo reenviar el informe. Intenta nuevamente.',
-            ], 500);
-        }
+        //     return response()->json([
+        //         'message' => 'No se pudo reenviar el informe. Intenta nuevamente.',
+        //     ], 500);
+        // }
             try {
               //  $reportNotificationService->notifyProcessReport($proceso, $storedPath, $originalName);
             } catch (\Throwable $e) {
