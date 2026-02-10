2979958f28f459c24c9a09a5153b17bd5a4767ba
calgreenex <carlos.alvarez@greenex.cl>
2026-02-10 15:52:45 -0300

    .

--- app/Http/Controllers/Api/FolioDeductionController.php
+++ app/Http/Controllers/Api/FolioDeductionController.php
@@ -7,6 +7,7 @@ use App\Models\FolioDeduction;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class FolioDeductionController extends Controller
 {
